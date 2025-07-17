7cc6f7ffd108eec5ac7cfbd5a67560439a5e7f92
Nguyễn Quang Huy <quanghuy19062002@gmail.com>
2025-07-17 16:20:39 +0700

    Delete TokenResponse.cs

--- ChamCongHaiChau.Shared/Models/TokenResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ChamCongHaiChau.Shared.Models;
-
-public class TokenResponse
-{
-    public string Token { get; set; } = string.Empty;
-    public ApplicationUser currentUser { get; set; } = new();
-}
